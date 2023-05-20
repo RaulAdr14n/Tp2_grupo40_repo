7a6681dca802e8e79bebdb5a8e102620bf375ee5
Raul Hernandez <Raul Hernandez@DESKTOP-2RBKAM1>
2023-05-20 17:55:36 -0300

    Añadido ProdcutosController - HeaderController

--- Tp2_grupo40/src/main/java/ar/edu/unju/fi/controller/model/ProductosController.java
+++ Tp2_grupo40/src/main/java/ar/edu/unju/fi/controller/model/ProductosController.java
@@ -1,5 +1,74 @@
 package ar.edu.unju.fi.controller.model;
 
-public class ProductosController {
+import org.springframework.stereotype.Controller;
 
+@Controller
+public class ProductosController {
+	private String nombre;
+	private int codigo;
+	private double precio;
+	private String categoria;
+	private int descuento;
+
+	// Constructor parametrizado
+	public ProductosController(String nombre, int codigo, double precio, String categoria, int descuento) {
+		this.nombre = nombre;
+		this.codigo = codigo;
+		this.precio = precio;
+		this.categoria = categoria;
+		this.descuento = descuento;
+	}
+
+	// Getters y setters
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public int getDescuento() {
+		return descuento;
+	}
+
+	public void setDescuento(int descuento) {
+		this.descuento = descuento;
+	}
+
+	// Método para calcular el descuento del producto
+	public double calcularDescuento() {
+		double precioConDescuento = precio;
+
+		if (descuento > 0 && descuento <= 50) {
+			double descuentoPorcentaje = descuento / 100.0;
+			precioConDescuento = precio - (precio * descuentoPorcentaje);
+		}
+
+		return precioConDescuento;
+	}
 }
